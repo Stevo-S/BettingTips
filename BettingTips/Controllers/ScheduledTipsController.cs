1a5398c92e52ce58aa6185d5ae2bf832dda4f1a9
Steve Savio <steve@bettersms.net>
2016-04-08 20:48:40 +0300

    Scaffold Scheduled Tips

---
+++ BettingTips/Controllers/ScheduledTipsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BettingTips.Models;
+
+namespace BettingTips.Controllers
+{
+    public class ScheduledTipsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ScheduledTips
+        public ActionResult Index()
+        {
+            return View(db.ScheduledTips.ToList());
+        }
+
+        // GET: ScheduledTips/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ScheduledTip scheduledTip = db.ScheduledTips.Find(id);
+            if (scheduledTip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scheduledTip);
+        }
+
+        // GET: ScheduledTips/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ScheduledTips/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,TipNumber,Tip,DateScheduled,Destination,ServiceId")] ScheduledTip scheduledTip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ScheduledTips.Add(scheduledTip);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(scheduledTip);
+        }
+
+        // GET: ScheduledTips/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ScheduledTip scheduledTip = db.ScheduledTips.Find(id);
+            if (scheduledTip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scheduledTip);
+        }
+
+        // POST: ScheduledTips/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,TipNumber,Tip,DateScheduled,Destination,ServiceId")] ScheduledTip scheduledTip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(scheduledTip).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(scheduledTip);
+        }
+
+        // GET: ScheduledTips/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ScheduledTip scheduledTip = db.ScheduledTips.Find(id);
+            if (scheduledTip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scheduledTip);
+        }
+
+        // POST: ScheduledTips/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ScheduledTip scheduledTip = db.ScheduledTips.Find(id);
+            db.ScheduledTips.Remove(scheduledTip);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
